39363e95cbc5fe370d41115ec5f5906be2b3ca07
XIMalik <abdulmalikawesu@gmail.com>
2023-02-24 09:14:35 +0100

    lay

--- src/App.jsx
+++ src/App.jsx
@@ -26,17 +26,17 @@ function App() {
 
     return (
         <div className="App flex flex-col gap-1 justify-center items-center w-[100%] bg-white">
-          <div className='py-5 sticky top-0 bg-white z-10 text-center w-[100%] m-2 border-b-[0.2px] border-solid border-gradient-to-r from-blue-500 to-red-500'>
+          <div className='py-5 sticky top-0 z-10 text-center w-[100%] m-2 border-b-[0.2px] border-solid bg-white/50 backdrop-blur-lg'>
             <h1 className=' text-slate-900 head text-5xl font-bold text- outline-1'> grfxn!66</h1>
-            <span className='text-sm text-slate-600'> Troll niggas</span>
+            <span className='text-sm text-slate-600'> Create custom graphics</span>
           </div>
 
-            <label htmlFor="inputag" className="input cursor-pointer gap-3 flex flex-col align-center items-center text-slate-600" >
+            <label htmlFor="inputag" className="input cursor-pointer gap-3 flex flex-col align-center items-center text-slate-600 " >
               <span className='w-[50px] text-sm text-center text-black rounded-full px-1 bg-slate-300'>1</span>Upload an image
               <input type="file" id="inputag" onChange={handleChange} className="hidden"/>
             </label>
           
-            <div ref={memeRef} id="meme" className='relative overflow-hidden w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] flex justify-center items-center rounded-md bg-gradient-to-bl from-blue-900 to-slate-200 mb-5 mt-5'>
+            <div ref={memeRef} id="meme" className='relative overflow-hidden w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] flex justify-center items-center rounded-md bg-gradient-to-bl from-white to-slate-200 mb-5 mt-5'>
               <h1 id="memetext" style={mystyle} className='text-3xl uppercase absolute top-2 font-black'>{topText}</h1>
               <img src={file} className="w-[100%] object-cover"/>
               <h1 id="memetext" style={mystyle} className='text-3xl uppercase absolute bottom-2 font-black'>{bottomText}</h1>
@@ -45,23 +45,25 @@ function App() {
             <div className=' flex flex-col justify-center my-5 items-center gap-3'>
               <span className='w-[50px] text-sm text-center text-black rounded-full px-1 bg-slate-300'>2</span>
               <span className='text-slate-600'>Change the text color <span className='text-xs text-slate-400'>if need be</span></span>
-                <div className='flex justify-center gap-8 md:gap-12 py-2'>
-                  <button onClick={() => setColor("green")} className="bg-green-600 w-[25px] h-[25px] rounded-full "></button>
-                  <button onClick={() => setColor(["lightblue"])} className="bg-red-600 w-[25px] h-[25px] rounded-full"></button>
-                  <button onClick={() => setColor("blue")} className="bg-blue-600 w-[25px] h-[25px] rounded-full"></button>
-                  <button onClick={() => setColor("white")} className="bg-white border-[0.5px] border-solid w-[25px] h-[25px] rounded-full"></button>
+                <div className='flex justify-center gap-8 sm:gap-8 py-2'>
+                  <button onClick={() => setColor("green")} className="bg-green-600 w-[25px] h-[25px] rounded-lg "></button>
+                  <button onClick={() => setColor(["lightblue"])} className="bg-red-600 w-[25px] h-[25px] rounded-lg"></button>
+                  <button onClick={() => setColor("blue")} className="bg-blue-600 w-[25px] h-[25px] rounded-lg"></button>
+                  <button onClick={() => setColor("white")} className="bg-white border-[0.5px] border-solid w-[25px] h-[25px] rounded-lg"></button>
+                  <button onClick={() => setColor("purple")} className="bg-purple-500 border-[0.5px] border-solid w-[25px] h-[25px] rounded-lg"></button>
+                  <button onClick={() => setColor("yellow")} className="bg-yellow-500 border-[0.5px] border-solid w-[25px] h-[25px] rounded-lg"></button>
                 </div>
             </div>
             
             
 
-            <div className='flex flex-col gap-5 justify-center items-center mt-5 w-[100%] min-w-[400px] px-5 md:px-2'>
-            <span className='w-[50px] text-sm text-center text-black rounded-full px-1 bg-slate-300'>3</span><span className='text-left'>Input some text to complete your <span className='head'>grfx</span></span>
+            <div className='flex flex-col gap-5 justify-center items-center mt-5 w-[100%] min-w-[400px] max-w-[600px] px-5 md:px-2'>
+            <span className='w-[50px] text-sm text-center text-black rounded-full px-1 bg-slate-300'>3</span><span className='text-left text-slate-600'>Input some text to complete your <span className='head'>grfx</span></span>
               <input placeholder="top text" type="text" id="topText" onChange={e => setTopText(e.target.value)} className=' bg-slate-100 ease-in duration-150 hover:bg-slate-200 border-0 outline-0 rounded-lg p-2 w-[50%] min-w-[400px]'/>
               <input placeholder="bottom text" type="text" id="bottomText" onChange={e => setBottomText(e.target.value)} className='bg-slate-100 ease-in duration-150 hover:bg-slate-200 border-0 outline-0 rounded-lg p-2 w-[50%] min-w-[400px]'/>
             </div>
               
-              <button className=' py-3 bg-slate-900 text-green-100 rounded-xl mt-5 min-w-[400px] ease-in duration-150 hover:bg-slate-800 mb-20' onClick={(e) => exportComponentAsJPEG(memeRef)}>Download your <span className='head'>grfx</span></button>
+              <button className=' py-3 bg-slate-900 text-green-100 rounded-xl mt-5 min-w-[400px] ease-in duration-150 hover:bg-slate-800 mb-20' onClick={(e) => exportComponentAsJPEG(memeRef)}>Download your custom <span className='head'>grfx</span></button>
 
               <div className="footer bg-slate-100 w-[100%] p-5">
                 <span className='text-sm text-slate-600 py-5'>Check out my other work</span> <br />
