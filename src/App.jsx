5ad54f60b1f397473acfa7a762922d329ff30e70
XIMalik <abdulmalikawesu@gmail.com>
2023-02-25 07:50:10 +0100

    updatev0.1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import Home from './pages/Home/Home';
 
 
 function App() {
+  const [count, setCount] = useState(0)
+  const goUp = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
+  
   return(
     <BrowserRouter>
       <Routes>
